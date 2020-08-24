36ad922833f274feb4078bfdafc2e119d7b3c130
Joseph Chen <joseph.chen@gmail.com>
2020-08-24 14:11:38 -0700

    Remove old file

--- spec/models/book/foreign_publication.rb
+++
@@ -1,9 +0,0 @@
-class Book
-  class ForeignPublication
-    include Mongoid::Document
-    include Mongoid::MagicCounterCache
-
-    belongs_to :book, :inverse_of => :foreign_publications
-    counter_cache :book
-  end
-end
